4c1134052f746d094a128846cd809693e225a193
osvaldsoza <osvald.soza@gmail.com>
2022-03-21 09:30:58 -0300

    cadastro de produto

---
+++ src/main/java/br/com/monktec/entity/CadastrarProdutoDTO.java
@@ -0,0 +1,26 @@
+package br.com.monktec.entity;
+
+import java.math.BigDecimal;
+
+public class CadastrarProdutoDTO {
+
+    private String nome;
+
+    private BigDecimal valor;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+}
--- src/main/java/br/com/monktec/resource/ProdutoResource.java
+++ src/main/java/br/com/monktec/resource/ProdutoResource.java
@@ -1,11 +1,10 @@
 package br.com.monktec.resource;
 
+import br.com.monktec.entity.CadastrarProdutoDTO;
 import br.com.monktec.entity.Produto;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -17,5 +16,16 @@ public class ProdutoResource {
     @GET
     public List<Produto> listAll(){
        return Produto.listAll();
+    }
+
+    @POST
+    @Transactional
+    public void novoProduto(CadastrarProdutoDTO produtoDTO){
+        Produto produto = new Produto();
+
+        produto.setNome(produtoDTO.getNome());
+        produto.setValor(produtoDTO.getValor());
+
+        produto.persist();
     }
 }
