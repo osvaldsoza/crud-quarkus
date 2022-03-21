cb4797bf768f384282305d8a0a7e86b3b7200090
osvaldsoza <osvald.soza@gmail.com>
2022-03-21 00:03:50 -0300

    first commit

---
+++ src/main/java/br/com/monktec/entity/Produto.java
@@ -0,0 +1,55 @@
+package br.com.monktec.entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Entity;
+import java.math.BigDecimal;
+import java.sql.Date;
+
+@Entity
+public class Produto extends PanacheEntity {
+
+    private String nome;
+
+    private BigDecimal valor;
+
+    @CreationTimestamp
+    private Date dataCriacao;
+
+    @UpdateTimestamp
+    private Date dataAtualizacao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Date getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(Date dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+}
---
+++ src/main/java/br/com/monktec/resource/ProdutoResource.java
@@ -0,0 +1,21 @@
+package br.com.monktec.resource;
+
+import br.com.monktec.entity.Produto;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("produtos")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ProdutoResource {
+
+    @GET
+    public List<Produto> listAll(){
+       return Produto.listAll();
+    }
+}
