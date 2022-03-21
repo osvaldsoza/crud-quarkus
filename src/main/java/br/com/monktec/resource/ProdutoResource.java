a7b2cb6a8f96fb91d3028959cbb3bd98f23a6247
osvaldsoza <osvald.soza@gmail.com>
2022-03-21 13:18:56 -0300

    atualiza e deleta  produto

--- src/main/java/br/com/monktec/resource/ProdutoResource.java
+++ src/main/java/br/com/monktec/resource/ProdutoResource.java
@@ -7,6 +7,7 @@ import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
+import java.util.Optional;
 
 @Path("produtos")
 @Produces(MediaType.APPLICATION_JSON)
@@ -14,18 +15,46 @@ import java.util.List;
 public class ProdutoResource {
 
     @GET
-    public List<Produto> listAll(){
-       return Produto.listAll();
+    public List<Produto> listAll() {
+        return Produto.listAll();
     }
 
     @POST
     @Transactional
-    public void novoProduto(CadastrarProdutoDTO produtoDTO){
+    public void novoProduto(CadastrarProdutoDTO produtoDTO) {
         Produto produto = new Produto();
 
         produto.setNome(produtoDTO.getNome());
         produto.setValor(produtoDTO.getValor());
 
         produto.persist();
+    }
+
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public void atualizaProduto(@PathParam("id") Long id, CadastrarProdutoDTO produtoDTO) {
+        Optional<Produto> produtoOp = Produto.findByIdOptional(id);
+
+        if (produtoOp.isPresent()) {
+            Produto produto = produtoOp.get();
+            produto.setNome(produtoDTO.getNome());
+            produto.setValor(produtoDTO.getValor());
+
+            produto.persist();
+        } else {
+            throw new NotFoundException();
+        }
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public void deleteProduto(@PathParam("id") Long id) {
+        Optional<Produto> produtoOp = Produto.findByIdOptional(id);
+
+        produtoOp.ifPresentOrElse(Produto::delete, () -> {
+            throw new NotFoundException();
+        });
     }
 }
